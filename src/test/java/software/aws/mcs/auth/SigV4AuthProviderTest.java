3c7de53fa13a5ef12259787ebb5c62d99617248c
Derek Chen-Becker <dchenbec@amazon.com>
2020-03-02 19:01:43 +0000

    Initial Commit of Code
    This is the initial commit of working code for the plugin, staged for release.

---
+++ src/test/java/software/aws/mcs/auth/SigV4AuthProviderTest.java
@@ -0,0 +1,91 @@
+package software.aws.mcs.auth;
+
+/*-
+ * #%L
+ * AWS SigV4 Auth Java Driver 4.x Plugin
+ * %%
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
+public class SigV4AuthProviderTest {
+    @Test
+    public void testNonceExtraction() {
+        final String TEST_NONCE = "1234abcd1234abcd1234abcd1234abcd";
+        // Valid nonce is 32 characters, so we test that here
+        assertEquals(ByteBuffer.wrap(TEST_NONCE.getBytes(StandardCharsets.UTF_8)),
+                     ByteBuffer.wrap(SigV4AuthProvider.extractNonce(ByteBuffer.wrap(("nonce=" + TEST_NONCE)
+                                                                                    .getBytes(StandardCharsets.UTF_8)))));
+    }
+
+    @Test
+    public void testShortNonceExtraction() {
+        assertThrows(IllegalArgumentException.class, () -> {
+                SigV4AuthProvider.extractNonce(ByteBuffer.wrap("nonce=too_short".getBytes(StandardCharsets.UTF_8)));
+            });
+    }
+
+    @Test
+    public void testNonceExtractionFailure() {
+        assertThrows(IllegalArgumentException.class, () -> {
+                SigV4AuthProvider.extractNonce(ByteBuffer.wrap("nothing to see here".getBytes(StandardCharsets.UTF_8)));
+            });
+    }
+
+    @Test
+    public void testSimpleIndexOf() {
+        byte[] target = {0, 1, 2, 42, 24, 4, 5};
+        byte[] pattern = {42, 24};
+        assertEquals(3, SigV4AuthProvider.indexOf(target, pattern));
+    }
+
+    @Test
+    public void testLeadingIndexOf() {
+        byte[] target = {42, 24, 1, 2, 3, 4, 5};
+        byte[] pattern = {42, 24};
+        assertEquals(0, SigV4AuthProvider.indexOf(target, pattern));
+    }
+
+    @Test
+    public void testTrailingIndexOf() {
+        byte[] target = {1, 2, 3, 4, 5, 42, 24};
+        byte[] pattern = {42, 24};
+        assertEquals(5, SigV4AuthProvider.indexOf(target, pattern));
+    }
+
+    @Test
+    public void testPartialIndexOf() {
+        byte[] target = {1, 2, 42, 24, 3, 4, 5};
+        byte[] pattern = {42, 24, 42};
+        assertEquals(-1, SigV4AuthProvider.indexOf(target, pattern));
+    }
+
+    @Test
+    public void testPartialTrailingIndexOf() {
+        byte[] target = {1, 2, 3, 4, 5, 42};
+        byte[] pattern = {42, 24, 42};
+        assertEquals(-1, SigV4AuthProvider.indexOf(target, pattern));
+    }
+
+}
---
+++ src/test/java/software/aws/mcs/auth/TestSigV4.java
@@ -0,0 +1,76 @@
+package software.aws.mcs.auth;
+
+/*-
+ * #%L
+ * AWS SigV4 Auth Java Driver 4.x Plugin
+ * %%
+ * Copyright (C) 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.*;
+
+import software.aws.mcs.auth.SigV4AuthProvider;
+
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import javax.net.ssl.SSLContext;
+
+public class TestSigV4 {
+    static String[] DEFAULT_CONTACT_POINTS = {"127.0.0.1:9042"};
+
+    public static void main(String[] args) throws Exception {
+        String[] contactPointsRaw = DEFAULT_CONTACT_POINTS;
+
+        if (args.length == 1) {
+            contactPointsRaw = args[0].split(",");
+        } else if (args.length > 1) {
+            System.out.println("Usage: TestSigV4 [<contact points, comma separated, 'IP:port' format>]");
+            System.exit(-1);
+        }
+
+        ArrayList<InetSocketAddress> contactPoints = new ArrayList<>(contactPointsRaw.length);
+
+        for (int i = 0; i < contactPointsRaw.length; i++) {
+            String[] parts = contactPointsRaw[i].split(":");
+            contactPoints.add(InetSocketAddress.createUnresolved(parts[0], Integer.parseInt(parts[1])));
+        }
+
+        System.out.println("Using endpoints: " + contactPoints);
+
+        // The CqlSession object is the main entry point of the driver.
+        // It holds the known state of the actual Cassandra cluster (notably the Metadata).
+        // This class is thread-safe, you should create a single instance (per target Cassandra cluster), and share
+        // it throughout your application.
+        try (CqlSession session = CqlSession.builder()
+             .addContactPoints(contactPoints)
+             .withAuthProvider(new SigV4AuthProvider())
+             .withSslContext(SSLContext.getDefault())
+             .withLocalDatacenter("dc1")
+             .build()) {
+
+            // We use execute to send a query to Cassandra. This returns a ResultSet, which is essentially a collection
+            // of Row objects.
+            ResultSet rs = session.execute("select release_version from system.local");
+            //  Extract the first row (which is the only one in this case).
+            Row row = rs.one();
+
+            // Extract the value of the first (and only) column from the row.
+            String releaseVersion = row.getString("release_version");
+            System.out.printf("Cassandra version is: %s%n", releaseVersion);
+        }
+    }
+}
